98a052533e4b63729cc6123bb4e4f33b6767ce45
z004mw9j <chunfeng.dou.ext@siemens.com>
2022-12-09 12:36:25 +0800

    test

--- rpc_api/src/main/java/com/dcf/api/HelloService.java
+++ rpc_api/src/main/java/com/dcf/api/HelloService.java
@@ -2,6 +2,7 @@ package com.dcf.api;
 
 import com.dcf.common.annotation.RpcService;
 
+
 @RpcService(serviceName = "rpc_service1")
 public interface HelloService {
     String sayHello(String name);
